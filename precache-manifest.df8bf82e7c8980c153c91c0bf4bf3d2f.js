875dff8aaa790c136a9d2b836bee19d7206d96d0
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-08-14 03:40:18 +0530

    Updates

---
+++ precache-manifest.df8bf82e7c8980c153c91c0bf4bf3d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ada41936d25506d7ad43fb0d042286d",
+    "url": "/react-color-generator/index.html"
+  },
+  {
+    "revision": "3f5aa5631a4a82b9a3a6",
+    "url": "/react-color-generator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/react-color-generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/react-color-generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5aa5631a4a82b9a3a6",
+    "url": "/react-color-generator/static/js/main.6c7dd8ae.chunk.js"
+  },
+  {
+    "revision": "86272262674149d27e8a",
+    "url": "/react-color-generator/static/js/runtime-main.9c11df16.js"
+  }
+]);
